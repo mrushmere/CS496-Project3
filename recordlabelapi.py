2ac1bbee4dc60a0b8d448555f5e82f9348354f4e
Mark Rushmere <Mark Rushmere>
2016-04-21 22:33:43 -0400

    initial commit

---
+++ recordlabelapi.py
@@ -0,0 +1,11 @@
+"""Hello World API implemented using Google Cloud Endpoints.
+
+Defined here are the ProtoRPC messages needed to define Schemas for methods
+as well as those methods defined in an API.
+"""
+
+
+import endpoints
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
